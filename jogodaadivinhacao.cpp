0da4afa01d0a63c37ede84ac140ce70bd3c2c6ea
Janile Dantas <janile.dantas@stone.com.br>
2024-05-25 19:40:16 -0300

    usando cout e endl

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main () {
+    
+    std::cout << "###########################" << std::endl;
+    std::cout << "#     Bem vindo           #" << std::endl;
+    std::cout << "###########################" << std::endl;
+}
