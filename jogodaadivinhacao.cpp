67cfc1f83f8f1086218fc962421c1edcc27260af
Janile Dantas <janile.dantas@stone.com.br>
2024-05-25 21:34:09 -0300

    usando o cin

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,5 +8,20 @@ int main () {
     cout << "###########################" << endl;
 
     int numero_secreto = 10;
-    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+
+    int chute;
+    cout << "Qual é o seu chute?";
+    cin >> chute;
+
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if(chute == numero_secreto) {
+        cout << "Parabéns! Você acertou o número secreto!" << endl;
+    } 
+    else if(chute > numero_secreto) {
+        cout << "Seu chute foi maior que o número secreto" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto" << endl; 
+    } 
 }
