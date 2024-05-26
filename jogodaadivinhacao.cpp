23cb06e5a6fecb0026a8b19e611c71c10bb0b08c
Janile Dantas <janile.dantas@stone.com.br>
2024-05-26 12:27:35 -0300

    usando constante e extraindo condições para variáveis

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -7,7 +7,7 @@ int main () {
     cout << "#     Bem vindo           #" << endl;
     cout << "###########################" << endl;
 
-    int numero_secreto = 10;
+    const int NUMERO_SECRETO = 10;
 
     int chute;
     cout << "Qual é o seu chute?";
@@ -15,10 +15,13 @@ int main () {
 
     cout << "O valor do seu chute é: " << chute << endl;
 
-    if(chute == numero_secreto) {
+    bool acertou = chute == NUMERO_SECRETO;
+    bool chute_maior = chute > NUMERO_SECRETO; 
+
+    if(acertou) {
         cout << "Parabéns! Você acertou o número secreto!" << endl;
     } 
-    else if(chute > numero_secreto) {
+    else if(chute_maior) {
         cout << "Seu chute foi maior que o número secreto" << endl;
     }
     else {
