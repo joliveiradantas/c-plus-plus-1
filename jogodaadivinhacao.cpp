0d12fe61fa28bbae88f56b53ae5ea390502badc1
Janile Dantas <janile.dantas@stone.com.br>
2024-05-25 21:05:11 -0300

    usando variáveis

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -6,4 +6,7 @@ int main () {
     cout << "###########################" << endl;
     cout << "#     Bem vindo           #" << endl;
     cout << "###########################" << endl;
+
+    int numero_secreto = 10;
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
 }
